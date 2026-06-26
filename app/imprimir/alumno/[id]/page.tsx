8bbb075b3134b52e2a429ef5989593fd8d35439f
nuevegol <guillear75@gmail.com>
2026-06-26 16:39:18 -0300

    Modulo impresiones completo

--- app/imprimir/alumno/[id]/page.tsx
+++ app/imprimir/alumno/[id]/page.tsx
@@ -64,7 +64,9 @@ const documentos = [
     ruta: "trayectoria",
   },
 ];
+
   return (
+
     <div className="min-h-screen bg-slate-100 p-8">
 
       <div className="max-w-7xl mx-auto">
---
+++ app/imprimir/valoraciones/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="p-10">
+      Módulo en construcción
+    </div>
+  );
+}
